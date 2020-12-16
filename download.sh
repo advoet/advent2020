a0fef087c4b4ded40273a73ff932830ed46cd93a
advoet <advoet@gmail.com>
2020-12-16 12:03:02 -0800

    download script

---
+++ download.sh
@@ -0,0 +1,8 @@
+if [ $(find "data/day$1.advent") ]
+then
+	echo "Day $1 already downloaded."
+else
+	SESSION=$(<"cookies")
+	curl -o data/day$1.advent https://adventofcode.com/2020/day/$1/input --cookie "session=$SESSION"
+	echo "Day $1 saved to /data/day$1.advent"
+fi
